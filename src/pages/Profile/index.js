f466fed6ad24bcb081ee3ad47dc825ae474ff86e
Satoh Jiro <trananhq2345@gmail.com>
2022-11-30 10:36:28 +0700

    change profile grammar

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -11,14 +11,14 @@ function Profile() {
             </div>
             <div className={cx('profile-detail')}>
                 <span>
-                    My name is Nguyen Tran Anh and I'm a Vietnamese. I'm now twenty-one years old. I live in Ho Chi Minh
+                    My name is Nguyen Tran Anh and I'm Vietnamese. I'm now twenty-one years old. I live in Ho Chi Minh
                     city in the south of Vietnam. I'm a generous and easy-going person but when it comes to work I'm a
                     competitive perfectionist. I'm also an optimistic and outgoing person so I have many friends and
                     other social relationships. My preference is for reading Manga, a style of Japanese comic books and
                     graphic novels and also love listening to music. I decided to become a Developer because have always
-                    been fascinated by technology and logic problems. My future goals is an opportunity to go abroad to
-                    make a difference in the community. Five years from now, I want to become a project manager of a
-                    Software, As a project manager, my primary goals are managing people, set budgets, and making
+                    been fascinated by technology and logic problems. My future goal is an opportunity to go abroad to
+                    make a difference in the community. Five years from now, I want to become a project manager of
+                    Software, As a project manager, my primary goals are managing people, setting budgets, and making
                     decisions of all kinds. I also want to speak English fluently so I can work with people from
                     different countries.
                 </span>
